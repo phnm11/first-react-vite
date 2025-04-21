986f7839747a75380a09bcb220695f230ec02253
Phạm Hoàng Nam <NamPH@vissoft.vn>
2025-04-22 01:30:25 +0700

    add header and footer

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>React Vite - @phnm11</p>
+        </div>
+
+    );
+}
+
+export default Footer;
