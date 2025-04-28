201ab5b453b7ae696c07bc838a581789a9fc2b91
Phạm Hoàng Nam <NamPH@vissoft.vn>
2025-04-28 16:35:04 +0700

    update user

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,14 +1,40 @@
-import { NavLink } from 'react-router-dom';
-import './header.css';
-const Header = () => {
-    return (
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/users">Users</NavLink></li>
-            <li><NavLink to="/books">Books</NavLink></li>
-        </ul>
+import { Link, NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
+import { HomeOutlined, TeamOutlined, AuditOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
-    );
+const Header = () => {
+    const [current, setCurrent] = useState('');
+
+    const onClick = e => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"}>Users</Link>,
+            key: 'users',
+            icon: <TeamOutlined />
+        },
+        {
+            label: <Link to={"/books"}>Books</Link>,
+            key: 'books',
+            icon: <AuditOutlined />
+        }
+    ];
+
+    return <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+    />;
 }
 
 export default Header;
---
+++ src/components/user/update.user.modal.jsx
@@ -0,0 +1,84 @@
+import { Input, Modal, notification } from "antd";
+import { useEffect, useState } from "react";
+import { createUserAPI, updateUserAPI } from "../../services/api.service";
+
+const UpdateUserModal = (props) => {
+    const { isModalUpdateOpen, setIsModalUpdateOpen, dataUpdate, setDataUpdate, loadUser } = props;
+
+    const [id, setId] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [phone, setPhone] = useState("");
+
+    // next dataUpdate != prev dataUpdate
+    useEffect(() => {
+        console.log(">>> check data update props", dataUpdate);
+        if (dataUpdate) {
+            setId(dataUpdate._id)
+            setFullName(dataUpdate.fullName);
+            setPhone(dataUpdate.phone);
+        }
+    }, [dataUpdate]);
+
+    const handleSubmitBtn = async () => {
+        const res = await updateUserAPI(id, fullName, phone);
+        if (res.data) {
+            notification.success({
+                message: "Update User",
+                description: "Cập nhật User thành công!"
+            })
+            resetAndCloseModal();
+            await loadUser();
+        } else {
+            notification.error({
+                message: "Error update User",
+                description: JSON.stringify(res.message)
+            })
+        }
+
+    }
+
+    const resetAndCloseModal = () => {
+        setIsModalUpdateOpen(false);
+        setId("")
+        setFullName("");
+        setPhone("");
+        setDataUpdate(null);
+    }
+
+    return (
+        <Modal
+            title="Update User"
+            open={isModalUpdateOpen}
+            onOk={() => handleSubmitBtn()}
+            onCancel={() => resetAndCloseModal()}
+            maskClosable={false}
+            okText={"Save"}
+        >
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                <div>
+                    <span>Id</span>
+                    <Input
+                        value={id}
+                        disabled
+                    />
+                </div>
+                <div>
+                    <span>Full name</span>
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Phone number</span>
+                    <Input
+                        value={phone}
+                        onChange={(event) => { setPhone(event.target.value) }}
+                    />
+                </div>
+            </div>
+        </Modal>
+    );
+}
+
+export default UpdateUserModal;
